c2ae251cf7ac9000fcb1120d597c002fb2f15e80
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-02 12:19:44 +0700

    feat: create useTrainSchedule custom hook

---
+++ app/hooks/useTrainSchedule.ts
@@ -0,0 +1,38 @@
+import { useMutation } from '@tanstack/react-query'
+import {
+  TrainScheduleParams,
+  TrainScheduleParamsSchema,
+  TrainScheduleResponse,
+} from '../schemas/krlStation'
+import axiosPublic from '../api/axios'
+
+const fetchTrainSchedule = async (
+  params: TrainScheduleParams
+): Promise<TrainScheduleResponse[]> => {
+  const parsed = TrainScheduleParamsSchema.safeParse(params)
+
+  if (!parsed.success) {
+    throw new Error('All forms must be filled in')
+  }
+
+  const { stationid, timefrom, timeto } = parsed.data
+
+  const response = await axiosPublic.get('/schedule', {
+    params: {
+      stationid,
+      timefrom,
+      timeto,
+    },
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+    },
+  })
+
+  return response.data.data
+}
+
+export const useTrainSchedule = () => {
+  return useMutation({
+    mutationFn: fetchTrainSchedule,
+  })
+}
