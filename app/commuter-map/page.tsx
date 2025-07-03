fc68685e8ad61f855261b3189e15849045e26024
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-03 14:38:28 +0700

    feat: change css styles

--- app/commuter-map/page.tsx
+++ app/commuter-map/page.tsx
@@ -18,7 +18,7 @@ const CommuterMap = () => {
       {/* ROUTE MAPS */}
       <div
         className={clsx(
-          'flex flex-col h-full overflow-y-auto p-3 no-scrollbar gap-4',
+          'flex flex-col flex-1 pb-[100px] sm:pb-5 overflow-y-auto px-3 no-scrollbar gap-4 mt-4',
           isLoading && 'justify-center items-center'
         )}
       >
