7c0db9875a7cc519e9b6df01cbe07cc8389b0c64
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-03 14:08:18 +0700

    feat: create commuter-map ui

--- app/commuter-map/page.tsx
+++ app/commuter-map/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Greeting from '../components/greeting'
 
 const CommuterMap = () => {
-  return <div>CommuterMap</div>
+  return (
+    <div className='relative flex flex-col h-screen'>
+      {/* GREETING */}
+      <Greeting descipription='Explore Our Route Network: Discover the Best Route for Every Destination' />
+    </div>
+  )
 }
 
 export default CommuterMap
