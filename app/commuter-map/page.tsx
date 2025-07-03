3a11f6b523031f23fe69fecf48b6eb25e49e5f1c
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-03 14:28:31 +0700

    feat: implement commuter-map api

--- app/commuter-map/page.tsx
+++ app/commuter-map/page.tsx
@@ -1,11 +1,41 @@
+'use client'
+
 import React from 'react'
 import Greeting from '../components/greeting'
+import { useCommuterMap } from '../hooks/useCommuterMap'
+import Loading from '../components/loading'
+import clsx from 'clsx'
 
 const CommuterMap = () => {
+  const { data, isLoading, error } = useCommuterMap()
+
   return (
     <div className='relative flex flex-col h-screen'>
       {/* GREETING */}
       <Greeting descipription='Explore Our Route Network: Discover the Best Route for Every Destination' />
+
+      {/* ROUTE MAPS */}
+      <div
+        className={clsx(
+          'flex flex-col h-full overflow-y-auto p-3 no-scrollbar gap-4',
+          isLoading && 'justify-center items-center'
+        )}
+      >
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            {data?.map((item, index) => (
+              <img
+                key={index}
+                src={item.permalink}
+                alt={`Area ${item.area}`}
+                className='border rounded-lg shadow-md'
+              />
+            ))}
+          </>
+        )}
+      </div>
     </div>
   )
 }
