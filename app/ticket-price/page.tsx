75ebbaa90eefee33053013659931ea68a698aa46
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-03 10:13:16 +0700

    feat: implement ticket-price ui

--- app/ticket-price/page.tsx
+++ app/ticket-price/page.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+'use client'
+
+import CommuterSearchPanel from '@/components/commuter-seacrh-panel'
+import Greeting from '@/components/greeting'
+import React, { useState } from 'react'
+import { useKrlStation } from '../hooks/useKrlStation'
+import { useToastOnError } from '../hooks/useToastOnError'
+import { TicketPrice as TypeTicketPrice } from '../schemas/ticket-price'
 
 const TicketPrice = () => {
-  return <div>TicketPrice</div>
+  const {
+    data: krlStationData,
+    isLoading: isKrlStationLoading,
+    isError: isKrlStationError,
+    error: krlStationError,
+  } = useKrlStation()
+
+  useToastOnError(krlStationError, isKrlStationError)
+
+  const [ticketPriceFormData, setTicketPriceFormData] =
+    useState<TypeTicketPrice>({
+      stationfrom: '',
+      stationto: '',
+    })
+
+  const handleFormObjectChange = (name: string, value: React.Key | null) => {
+    setTicketPriceFormData((current) => ({
+      ...current,
+      [name]: value,
+    }))
+  }
+
+  const handleCheckTicketPrice = () => {
+    // mutate(searchScheduleFormObject)
+  }
+
+  const isCheckTicketPriceFormValid = true
+  console.log(ticketPriceFormData)
+  return (
+    <div className='relative flex flex-col h-screen'>
+      {/* GREETING */}
+      <Greeting />
+
+      {/* SEARCH PANEL */}
+      <CommuterSearchPanel
+        isKrlStationLoading={isKrlStationLoading}
+        handleFormObjectChange={handleFormObjectChange}
+        handleSubmitButtonClick={handleCheckTicketPrice}
+        krlStationData={krlStationData}
+        isSubmitButtonEnabled={isCheckTicketPriceFormValid}
+        isButtonSubmitLoading={false}
+        variant='ticket-price'
+        title='Find the Best Fares for Your Journey'
+      />
+    </div>
+  )
 }
 
 export default TicketPrice
