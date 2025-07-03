0afb28149f98b9126f36e477af525d7f60752d04
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-03 11:38:27 +0700

    feat: create ticket price schema

--- app/schemas/ticket-price.ts
+++ app/schemas/ticket-price.ts
@@ -1,8 +1,19 @@
 import { z } from 'zod'
 
 export const TicketPriceSchema = z.object({
-  stationfrom: z.string(),
-  stationto: z.string(),
+  stationfrom: z.string().min(2),
+  stationto: z.string().min(2),
 })
 
 export type TicketPrice = z.infer<typeof TicketPriceSchema>
+
+export const TicketPriceResponseSchema = z.object({
+  sta_code_from: z.string(),
+  sta_name_from: z.string(),
+  sta_code_to: z.string(),
+  sta_name_to: z.string(),
+  fare: z.number(),
+  distance: z.string(),
+})
+
+export type TicketPriceResponse = z.infer<typeof TicketPriceResponseSchema>
