53ba147d71bb53065b18c1878c5f01ce38bad6d9
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-04 20:37:15 +0700

    style: update greeting styles

--- app/components/greeting.tsx
+++ app/components/greeting.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import moment from 'moment'
+import { Icon } from '@iconify/react'
+import clsx from 'clsx'
+
+import { useAppContext } from '../context/app-context'
 
 import { getTimeOfDay } from '@/app/lib/time'
 
 const Greeting = ({ descipription }: { descipription?: string }) => {
+  const { isSearchPanelExpanded, setIsSearchPanelExpanded } = useAppContext()
+
   return (
     <div className='min-h-[250px] bg-primary-500 rounded-b-3xl pt-8 flex flex-col items-center'>
       <div className='text-white'>
@@ -17,6 +23,18 @@ const Greeting = ({ descipription }: { descipription?: string }) => {
         <p className='mt-6 w-[365px] text-center text-default-200 font-medium text-2xl'>
           &quot;{descipription}&quot;
         </p>
+      )}
+
+      {descipription && (
+        <Icon
+          className={clsx(
+            'cursor-pointer text-white transition-all duration-500',
+            isSearchPanelExpanded ? 'rotate-180' : 'rotate-0'
+          )}
+          fontSize={30}
+          icon='icon-park-solid:down-one'
+          onClick={() => setIsSearchPanelExpanded(!isSearchPanelExpanded)}
+        />
       )}
     </div>
   )
