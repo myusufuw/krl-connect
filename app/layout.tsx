9e09fa0cb39ccbc7fa26541acc749de7d78067ed
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-04 20:35:26 +0700

    style: update layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,30 +2,52 @@
 
 import '@/styles/globals.css'
 import clsx from 'clsx'
+import { useEffect } from 'react'
 
 import { Providers } from './providers'
+import Tab from './components/tab'
 
 import { fontSans } from '@/config/fonts'
-import Tab from '@/app/components/tab'
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
+  useEffect(() => {
+    const setViewportHeight = () => {
+      const vh = window.innerHeight * 0.01
+
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+
+    setViewportHeight()
+    window.addEventListener('resize', setViewportHeight)
+
+    return () => window.removeEventListener('resize', setViewportHeight)
+  }, [])
+
   return (
     <html suppressHydrationWarning lang='en'>
-      <head />
+      <head>
+        <meta
+          content='width=device-width, initial-scale=1, user-scalable=no'
+          name='viewport'
+        />
+      </head>
       <body
         className={clsx(
-          'min-h-screen text-foreground bg-background font-sans antialiased',
-          fontSans.variable,
+          'text-foreground bg-background font-sans antialiased',
+          fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
-          <div className='relative flex flex-col h-screen bg-slate-900 overflow-hidden'>
+        <Providers themeProps={{ attribute: 'class', defaultTheme: 'light' }}>
+          <div className='flex flex-col bg-slate-900 overflow-hidden'>
             {/* <Navbar /> */}
-            <main className='container mx-auto max-w-md h-full relative bg-white zoom-90'>
+            <main
+              className='container mx-auto max-w-md relative bg-white'
+              style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
+            >
               {children}
 
               <Tab />
